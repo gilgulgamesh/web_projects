2b18651ef6fc61b321f1315e46dc75f36fda73d4
gilgulgalgal <gilgulgalgal@gmail.com>
2026-06-20 13:10:00 +1300

    got editor good, strted servering

---
+++ server.jl
@@ -0,0 +1,29 @@
+using Mongoose
+
+"""
+read("neocities-pimkossible\\index.html", String)
+
+req.body # can assig to a glloal variable
+
+assume js transcodes everything correctly. use json with name title and content
+
+"""
+content = ""
+
+router = Router()
+route!(router, :post, "/", req -> begin
+    global content
+    content = req.body
+    if content == "I'm valid"
+        Response(Plain, "That works")
+    else
+        Response(Plain, "That don't")
+    end
+end)
+
+server = Async(router) #either Server or Async
+start!(server, port=8080, blocking=false) # blocking=false lets me use repl
+
+# s() = shutdown!(server)
+# i() = include("server.jl")
+# si() = s();i();

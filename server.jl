933927e63436ce4cdd03d0387d1b1576b3363dd5
gilgulgalgal <gilgulgalgal@gmail.com>
2026-06-23 20:15:02 +1300

    server real good now. logs everything except trace and options ig

--- server.jl
+++ server.jl
@@ -1,55 +1,73 @@
 using Mongoose
 using Dates
 
-shutdown!(server)
+date() = replace(string(Dates.now()), "T" => " at ")
+restarted = false
 
-
-function addpost!(nextpost_text::String)
-   # get what's needed
-   old_db_text = read("playground/database.txt", String) # generalise, learn how to files
-
-   #time
-   postdate = Dates.now()
-   datestring = replace(string(postdate), r":" => s";")
-
-   #back up
-   backup_name = """playground\\database_before-""" * datestring * ".txt"
-   backup_file = open(backup_name, "w")
-   write(backup_file, old_db_text)
-   close(backup_file)
-
-   # splice
-   new_db_file = open("database.txt", "w")
-   write(new_db_file, nextpost_text * "\n\n" * old_db_text) # make elaborate
-   close(new_db_file)
+if @isdefined(server)
+    shutdown!(server)
+    restarted = true
+    close(LOG)
 end
 
-x = nothing
-u() = println("yo")
-# executes
+const LOG = open("output.txt", "a")
+
+if restarted
+    println("\nRebooted on $(date())")
+    write(LOG, "\nRebooted on $(date())")
+else
+    println("\nTurned on $(date())")
+    write(LOG, "\nTurned on $(date())")
+end
+
+function addpost!(nextpost::String)
+   db_loc = "playground/database.txt"
+   dbstring = read(db_loc, String)
+   dbfile = open(db_loc, "w")
+   write(dbfile, nextpost * "\n" * dbstring)
+   close(dbfile)
+end
+
+function wraplog!(req)
+
+   a = "\n   $(req.method) -> $(req.uri)"
+   b = "\n   ------"
+   c = "\n   === $(string(req.body))"
+   d = "\n   ------"
+   e = "\n   $(string(req))"
+   f = "\n   $(date()) \n   "
+
+   out = *(a, b, c, d, e, f)
+   write(LOG, out)
+
+   print(out)
+   Response(Plain, "404 Not Found"; status=404)
+end
+
+
+# # executes
 router = Router()
+route!(router, :get, "/", req -> wraplog!(req))
+route!(router, :put, "/", req -> wraplog!(req))
+route!(router, :delete, "/", req -> wraplog!(req))
+route!(router, :patch, "/", req -> wraplog!(req))
 route!(router, :post, "/", req -> begin
    addpost!(req.body)
-   new_db = read("database.txt", String)
-   println(req.body)
-   Response(Plain, new_db)
+   wraplog!(req)
+   valid = "Title=Hi &User=Me &Content=My post"
+   req.body == valid ? Response(Plain, "Ok") : Response(Plain, "400 Bad Request"; status=400)
 end)
 
-# route!(router, :get, "/", req -> begin
-#    println(req)
-#    Response(Plain, "ok")
-# end)
+route!(router, :post, "*", req -> wraplog!(req))
+route!(router, :get, "*", req -> wraplog!(req))
+route!(router, :put, "*", req -> wraplog!(req))
+route!(router, :delete, "*", req -> wraplog!(req))
+route!(router, :patch, "*", req -> wraplog!(req))
 
-config = Config(nworkers=8, request_timeout=15_000, max_body=5_242_880)
+config = Config(max_body=5_242_880)
+server = Server(router, config)
 
-server = Async(router, config)
-plug!(server, ratelimit(max_requests=10, window_seconds=30)) # Stricter limits
-plug!(server, health())
+plug!(server, ratelimit(max_requests=10, window_seconds=30))
+plug!(server, logger(output=LOG))
+plug!(server, logger())
 start!(server; host="0.0.0.0", port=8080, blocking=false)
-
-# definitions
-s() = shutdown!(server)
-si() = begin
-   s()
-   include("server.jl")
-end

6098f119316921447752236a9b94a4df0442e3e4
gilgulgalgal <gilgulgalgal@gmail.com>
2026-06-27 12:48:21 +1300

    htmlsanitizier. i think i got upload workingg?

--- parser.jl
+++ parser.jl
@@ -1,14 +1,12 @@
-using Dates, DelimitedFiles, Hyperscript #my fork
-
+using Dates
 
 HTML_FILE = "pimkossible/posts.html"
 banner = "Feng Shui of Life Blog"
-
-
 TSV_FILE = "posts.tsv"
+EVIL_TSV_FILE = "EVILposts.tsv"
+
 function cutedate(dt)
     typeof(dt) == String && return dt
-
     a = Dates.format(dt, "p-d-u'yy") # use time of day to get 🌇🏙️🌆 or smn
     b = lowercase(string(a))
     c = replace( b,
@@ -20,76 +18,91 @@ function cutedate(dt)
     )
 end
 
+using DelimitedFiles, HTMLSanitizer, Hyperscript #my fork
 
 function addpost!(postbody::String)
-    prior = readdlm(TSV_FILE, '\t', AbstractString)
-    columns =  size(prior, 1) # 1 is columns, including headers so does +1
+    prior = readdlm(TSV_FILE, '\t', String)
+    columns =  size(prior, 1) # including header  does +1 (1 arg is dims)
     row = makerow(postbody, columns)
-    # if row[2:end] == prior[end, 2:end]
-    #     return Response(Plain, "we got it last time babes", status=400)
-    # end
-    saverow(row)
+    if row[2:end] == prior[end, 2:end]
+        return Response(Plain, "we got it last time babes", status=400)
+    end
+    saverow(row, makesafe(row))
     updatehtml()
-    Response(Plain, "yay"; status=200)
+    sendwait()
+    Response(Link, "yay"; status=200)
 end
 
 function makerow(postbody::String, columns::Int)
-    title, user, content = match(r"&Title=(.+)&User=(.+)&Content=(.+)", postbody).captures
-
+    title, user, content = match(r"Title=(.+)&User=(.+)&Content=(.+)", postbody).captures
     date = cutedate(Dates.now())
     postnumber = "#$columns"
-    row::Vector{String} = [postnumber; title; user; date; content]
+    row::Vector = [
+        postnumber
+        title
+        user
+        date
+        content
+    ]
 end
 
-function saverow(row)
-    io = open(TSV_FILE,  "a")
-    writedlm(io, permutedims(row), '\t')
-    close(io)
+function makesafe(row::Vector)
+    # upgrade to dompurify
+    # consider limiting the limited one more for sanity
+    saferow::Vector = [
+        sanitize(row[1], whitelist = HTMLSanitizer.LIMITED)
+        sanitize(row[2], whitelist = HTMLSanitizer.LIMITED)
+        sanitize(row[3], whitelist = HTMLSanitizer.LIMITED)
+        sanitize(row[4], whitelist = HTMLSanitizer.LIMITED)
+        sanitize(row[5], whitelist = HTMLSanitizer.WHITELIST)
+    ]
 end
 
 
-@tags html head meta body style h1 h2 h4 span article  link div
-@tags_noescape div article section #test test if stock works on section , when article is a string anyway
+function saverow(row::Vector,  saferow::Vector)
+    io1 = open(TSV_FILE,  "a")
+    writedlm(io1, permutedims(saferow), '\t')
+    close(io1)
 
+    if row != saferow
+        println("UNSAFE POST CENSORED")
+        io2 = open(EVIL_TSV_FILE,  "a")
+        writedlm(io2, permutedims(row), '\t')
+        close(io2)
+    end
+end
 
-# syntax: row = ["hi"; "my"; "name"; "is"; "john"]
-
+@tags html head meta body style h1 h2 h4 span article  link
+@tags_noescape div article section #can this possible work recursively with stock...
 function updatehtml()
     data, headerrow  = readdlm(TSV_FILE, '\t', String, header=true)
     allposts = []
+
     for i in 1:size(data, 1)
         d = reverse(data, dims=1)[i, 1:5]
         push!(allposts, postbox(d))
     end
-    savehtml(HTML_FILE, Pretty(docubox(allposts)))
 
-    r = replace( read(HTML_FILE, String),
+    w = string(Pretty(docubox(allposts)))
+    r = replace(w,
         "<html><html>" => "<html>",
-        # """<section id="posts">""" => """<section id="posts">
-        # """,
-        # "</article>" => """</article>
-        # """,
         "</html></html>" => "</html>" )
-    io = open(HTML_FILE, "w")
-    write(io, r)
-    close(io)
-    println("good")
+    write(HTML_FILE, r)
 end
-
-#omfg i just want to cat, save, flip
+#omfg i just want to cat, save, flip save flip cat. file systems are not arrays. good for personal things.
 
 const postbox(row) =
-    article(id=row[1][2:end], #shoulda kept it without the # but ugh
-        h2.heading(
-            span(row[1] * " "),
-            span.Title(row[2])
-        ),
-        h4.subheading(
-            span.User(row[3]),
-            span(" " * cutedate(row[4]))
-        ),
-        div.Content(row[5])
-    )
+article(id=row[1][2:end], #shoulda kept it without the # but ugh
+    h2.heading(
+        span(row[1] * " "),
+        span.Title(row[2])
+    ),
+    h4.subheading(
+        span.User(row[3]),
+        span(" " * cutedate(row[4]))
+    ),
+    div.Content(row[5])
+)
 
 const docubox(allposts) =
 html(
@@ -101,17 +114,6 @@ html(
         h1(id="banner", banner),
         section(id="posts",
             allposts,
-            ),
         ),
-    )
-
-
-
-# a = []
-# for i in length(Posts.posts):-1:1
-#      push!(a, postbox(Posts.posts[i]))
-# end
-
-# savehtml(output, Pretty(docubox(a)))
-
-# data, headers = readdlm("posts.tsv", '\t' , String, '\n'; header=true)
+    ),
+)
--- server.jl
+++ server.jl
@@ -3,16 +3,12 @@ using Dates
 include("parser.jl")
 
 getdate() =  replace(string(Dates.now()), "T" => " at ")
-
-
 restarted = false
-
 if @isdefined(server)
     shutdown!(server)
     restarted = true
     close(LOG)
 end
-
 const LOG = open("output.txt", "a")
 
 if restarted
@@ -23,7 +19,6 @@ else
     write(LOG, "\nTurned on $(getdate())")
 end
 
-
 function wraplog!(req)
    a = "\n   $(req.method) -> $(req.uri)"
    b = "\n   ------"
@@ -31,56 +26,39 @@ function wraplog!(req)
    d = "\n   ------"
    e = "\n   $(string(req))"
    f = "\n   $(getdate()) \n   "
-
    out = *(a, b, c, d, e, f)
    write(LOG, out)
-
    print(out)
    Response(Plain, "404 Not Found"; status=404)
 end
 
-
-
-
-
-
-# # executes
+#  execute
 router = Router()
-
 route!(router, :post, "/", req-> begin
     wraplog!(req)
-
-    if match(r"&Title=(.+)&User=(.+)&Content=(.+)", req.body) === nothing
+    if match(r"Title=(.+)&User=(.+)&Content=(.+)", req.body) === nothing
         return Response(Plain, "Bad HTTP formatting"; status=400)
     end
     # try
-    addpost!(req.body)
+        addpost!(req.body)
     # catch
-        # Response(Plain, "something went wrong"; status=400)
+    #     Response(Plain, "something went wrong"; status=400)
     # end
 end)
 
-
-
-
-
-function logall()
-    route!(router, :get, "/", req -> wraplog!(req))
-    route!(router, :put, "/", req -> wraplog!(req))
-    route!(router, :delete, "/", req -> wraplog!(req))
-    route!(router, :patch, "/", req -> wraplog!(req))
-    route!(router, :head, "/", req -> wraplog!(req))
-    route!(router, :options, "/", req -> wraplog!(req))
-    route!(router, :get, "*", req -> wraplog!(req))
-    route!(router, :put, "*", req -> wraplog!(req))
-    route!(router, :patch, "*", req -> wraplog!(req))
-    route!(router, :delete, "*", req -> wraplog!(req))
-    route!(router, :head, "*", req -> wraplog!(req))
-    route!(router, :options, "*", req -> wraplog!(req))
-    route!(router, :post, "*", req -> wraplog!(req))
-end
-
-logall()
+route!(router, :get, "/", req -> wraplog!(req))
+route!(router, :put, "/", req -> wraplog!(req))
+route!(router, :delete, "/", req -> wraplog!(req))
+route!(router, :patch, "/", req -> wraplog!(req))
+route!(router, :head, "/", req -> wraplog!(req))
+route!(router, :options, "/", req -> wraplog!(req))
+route!(router, :get, "*", req -> wraplog!(req))
+route!(router, :put, "*", req -> wraplog!(req))
+route!(router, :patch, "*", req -> wraplog!(req))
+route!(router, :delete, "*", req -> wraplog!(req))
+route!(router, :head, "*", req -> wraplog!(req))
+route!(router, :options, "*", req -> wraplog!(req))
+route!(router, :post, "*", req -> wraplog!(req))
 
 config = Config(max_body=5_242_880)
 server = Server(router, config)
@@ -89,3 +67,8 @@ plug!(server, ratelimit(max_requests=10, window_seconds=30))
 plug!(server, logger(output=LOG))
 plug!(server, logger())
 start!(server; host="0.0.0.0", port=8080, blocking=false)
+
+
+function sendwait()
+     run(`pimkosible/upload.bat`) #add limiter!
+end
